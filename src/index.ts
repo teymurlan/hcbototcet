80c154bc0a5b40e243e759609b9d3483688612df
teymurlan <teymurlan@bk.ru>
2026-09-06 01:21:36 +0300

    fix: force Telegram WebApp to use current Worker origin

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-export { default } from './photo';
+import photo from './photo';
+import type { Env } from './photo';
+
 export type { Env } from './photo';
+
+export default {
+  fetch(req: Request, env: Env): Promise<Response> {
+    const origin = new URL(req.url).origin;
+    return photo.fetch(req, { ...env, WEBAPP_URL: origin });
+  },
+};
