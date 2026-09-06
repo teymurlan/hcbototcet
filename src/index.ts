80e03e0c267e6dde46c591430b2ac24725a8b567
teymurlan <teymurlan@bk.ru>
2026-09-06 16:10:28 +0300

    fix: route production entry through safe start handler

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import photo from './photo-v17';
-import type { Env } from './photo-v17';
+import photo from './photo-v17-startfix';
+import type { Env } from './photo-v17-startfix';
 
-export { AppState } from './photo-v17';
-export type { Env } from './photo-v17';
+export { AppState } from './photo-v17-startfix';
+export type { Env } from './photo-v17-startfix';
 
 export default {
   fetch(req: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
