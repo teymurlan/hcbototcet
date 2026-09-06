7c7bea611b0e70bd3f9828d330f62e7581c19928
teymurlan <teymurlan@bk.ru>
2026-09-06 11:30:46 +0300

    chore: wire v15 on test branch

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import photo from './photo-v14';
-import type { Env } from './photo-v14';
+import photo from './photo-v15';
+import type { Env } from './photo-v15';
 
-export { AppState } from './photo-v14';
-export type { Env } from './photo-v14';
+export { AppState } from './photo-v15';
+export type { Env } from './photo-v15';
 
 export default {
   fetch(req: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
