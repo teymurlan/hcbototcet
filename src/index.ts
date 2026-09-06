41f7ac4a50c1e0e21bb02f4a40a453bd6925db62
teymurlan <teymurlan@bk.ru>
2026-09-06 05:00:16 +0300

    fix: switch production to v9 GPS flow

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import photo from './photo-v8';
-import type { Env } from './photo-v8';
+import photo from './photo-v9';
+import type { Env } from './photo-v9';
 
-export { AppState } from './photo-v8';
-export type { Env } from './photo-v8';
+export { AppState } from './photo-v9';
+export type { Env } from './photo-v9';
 
 export default {
   fetch(req: Request, env: Env): Promise<Response> {
