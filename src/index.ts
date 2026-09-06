639b0d5d5827df63b758a4186898433624fafca6
teymurlan <teymurlan@bk.ru>
2026-09-06 11:32:40 +0300

    chore: point test branch to final v15

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import photo from './photo-v15';
-import type { Env } from './photo-v15';
+import photo from './photo-v15-final';
+import type { Env } from './photo-v15-final';
 
-export { AppState } from './photo-v15';
-export type { Env } from './photo-v15';
+export { AppState } from './photo-v15-final';
+export type { Env } from './photo-v15-final';
 
 export default {
   fetch(req: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
