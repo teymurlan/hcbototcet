0386f9865a55369cce2572cc0cbebc048162197d
teymurlan <teymurlan@bk.ru>
2026-09-16 12:23:54 +0300

    Use hardened parallel report STAFF entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import staff from './staff-operations';
-import type { Env } from './staff-operations';
+import staff from './staff-operations-safe';
+import type { Env } from './staff-operations-safe';
 
-export { AppState } from './staff-operations';
-export type { Env } from './staff-operations';
+export { AppState } from './staff-operations-safe';
+export type { Env } from './staff-operations-safe';
 
 export default {
   fetch(req: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
